56d7cbd09aa1038b3dcd88ea1fff30f1e654389a
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2026-03-12 23:37:52 +0300

    first

---
+++ alpha-panel/web/httpdocs/resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title inertia>{{ config('app.name', 'AlphaPanel') }}</title>
+    <link rel="manifest" href="/manifest.json">
+    <link rel="icon" type="image/svg+xml" href="/img/AlphaPanel-dark.svg">
+    @routes
+    @vite(['resources/css/app.css', 'resources/js/app.ts'])
+    @inertiaHead
+</head>
+<body class="font-sans antialiased overflow-x-hidden bg-white dark:bg-gray-900">
+    @inertia
+</body>
+</html>
