8f4cd95ba6bd3511c98a9acec69966c325ced018
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2026-03-20 23:58:28 +0300

    update

--- alpha-panel/web/httpdocs/app/Http/Controllers/FirewallController.php
+++ alpha-panel/web/httpdocs/app/Http/Controllers/FirewallController.php
@@ -59,7 +59,7 @@ class FirewallController extends Controller
             'action' => ['required', Rule::in(['ACCEPT', 'DROP', 'REJECT'])],
             'protocol' => ['required', Rule::in(['tcp', 'udp', 'icmp', 'all'])],
             'sources' => ['nullable', 'array'],
-            'sources.*' => ['string', 'ip'],
+            'sources.*' => ['string', 'regex:/^(\d{1,3}\.){3}\d{1,3}(\/\d{1,2})?$/'],
             'ports' => ['nullable', 'array'],
             'ports.*' => ['integer', 'min:1', 'max:65535'],
             'comment' => ['nullable', 'string', 'max:255'],
